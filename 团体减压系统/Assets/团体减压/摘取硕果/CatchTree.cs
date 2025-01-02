ea8e5f2f0c8fdb701e1aa69318477f3213a5bca7
huafeng <ai29027623@vip.qq.com>
2025-01-02 16:42:07 +0800

    extend player tween about catch apple. TODO:calculate duration time and use average time to show tween.

--- 团体减压系统/Assets/团体减压/摘取硕果/CatchTree.cs
+++ 团体减压系统/Assets/团体减压/摘取硕果/CatchTree.cs
@@ -1,13 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using UnityEditor.Animations;
 using UnityEngine;
 /*转为UTF-8*/
 public class CatchTree : GameLogic
 {
+
+    private Animator m_Animator;
+
+    public GameObject Apple;
+    private void Start()
+    {
+        m_Animator = UserModelList[0].GetComponent<Animator>();
+        //m_Animator.GetBehaviour<jumpControl>().OnFailingDownEvent += CatchTree_OnJumpDown;
+        //m_Animator.GetBehaviour<jumpControl>().OnDownEvent += CatchTree_OnDownEvent; ;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Jump();
+        }
+    }
+    private void CatchTree_OnDownEvent()
+    {
+        Apple.SetActive(false);
+    }
+
+    private void CatchTree_OnJumpDown()
+    {
+        Apple.SetActive(true);
+    }
+
     protected override void Instance_OnCompetitiveHeartValueChangeEvent(List<UserStateDetect.UserData> list)
     {
-        
+        //var data= list.Find(x => x.userID == 0);
+        //if (data!=null)
+        //{
+        //    if (data.heartValue>0)
+        //    {
+        //        JumpUp();
+        //    }
+        //}
+        //一首凤凰于飞把记忆拉回到了那个夏天，热腾腾的牛奶，出租屋带着凉意的水泥地，傍晚昏黄的路灯
     }
 
     protected override void Instance_OnGameStartEvent()
@@ -19,4 +58,9 @@ public class CatchTree : GameLogic
     {
         base.Instance_OnUserLenghChangeEvent(list, notifyCollectionChangedEventArgs);
     }
+    public void Jump()
+    {
+        m_Animator.Play("JumpPick");
+    }
+
 }
--- 团体减压系统/Assets/团体减压/摘取硕果/jumpControl.cs
+++ 团体减压系统/Assets/团体减压/摘取硕果/jumpControl.cs
@@ -1,13 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEngine;
 /*转为UTF-8*/
 public class jumpControl : StateMachineBehaviour
 {
+    public float process;
+
+    protected float jumpHeight = 9.998163f;
+
+    public float addHeight = 0;
+    /// <summary>
+    /// 下落事件
+    /// </summary>
+    public event Action OnFailingDownEvent;
+
+    /// <summary>
+    /// 落地事件
+    /// </summary>
+    public event Action OnDownEvent;
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         //调试根据动画状态机的状态来判断是否在地面上
         //并给出慢放和快放的速度
+        process=stateInfo.normalizedTime%1;
+        if (process>=0.35f&&process<0.55f)
+        {
+            //animator.speed=0.05f;
+            //OnJumpUp?.Invoke();
+            //animator.transform.position += Vector3.up * 0.002f;
+            float Targetheight=jumpHeight+addHeight;
+            float currentHeight = jumpHeight+ addHeight * ((process - 0.35f) / (0.55f - 0.35f));
+            Vector3 currentPos = animator.transform.position;
+            animator.transform.position = new Vector3(currentPos.x,currentHeight,currentPos.z);
+        }
+        else if (process >= 0.55f && process < 0.8f)
+        {
+            //animator.speed = 0.05f;
+            //OnJumpDown?.Invoke();
+            //animator.transform.position -= Vector3.up * 0.008f;
+            //14*12
+            float currentHeight =(addHeight+jumpHeight)- addHeight * ((process - 0.55f) / (0.8f - 0.55f));
+            Vector3 currentPos = animator.transform.position;
+            animator.transform.position = new Vector3(currentPos.x, currentHeight, currentPos.z);
+
+            OnFailingDownEvent?.Invoke();
+        }
+        //else if(process>0.86f)
+        //{
+        //    OnDownEvent?.Invoke();
+        //    //animator.speed = 0.3f;
+        //}
     }
 }
